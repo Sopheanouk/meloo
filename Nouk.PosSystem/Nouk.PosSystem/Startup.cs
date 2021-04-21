749e203770647fa98d84c440484f9fad122eb7bf
sopheanouk <pheanouk007@gmail.com>
2021-04-21 20:38:27 +0700

    Add mvc

--- Nouk.PosSystem/Nouk.PosSystem/Startup.cs
+++ Nouk.PosSystem/Nouk.PosSystem/Startup.cs
@@ -16,6 +16,7 @@ namespace Nouk.PosSystem
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,22 @@ namespace Nouk.PosSystem
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from the other side!");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from the other side!");
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
